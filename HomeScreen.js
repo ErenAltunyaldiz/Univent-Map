64905c88b7ad93809986d52b4f7271bd3f17b465
ErenAltunyaldiz <errneet@hotmail.com>
2024-05-31 21:26:14 +0300

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Etkinlik Uygulaması</Text>
-      <Button title="Etkinlik Oluştur" onPress={() => navigation.navigate('Map', { action: 'create' })} />
-      <Button title="Oluşturulan Etkinliğe Katıl" onPress={() => navigation.navigate('Map', { action: 'join' })} />
+    <View style={[styles.container, { backgroundColor: '#c7ffcf' }]}>
+      <Text style={styles.title}>Univent-Map</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Etkinlik Oluştur" onPress={() => navigation.navigate('Map', { action: 'create' })} />
+        <Button title="Oluşturulan Etkinliğe Katıl" onPress={() => navigation.navigate('Map', { action: 'join' })} />
+      </View>
+      <View style={styles.iconContainer}>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesome name="user" size={30} color="black" />
+          <Text>Profil</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesome name="bell" size={30} color="black" />
+          <Text>Bildirimler</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesome name="envelope" size={30} color="black" />
+          <Text>Mesajlar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesome name="cog" size={30} color="black" />
+          <Text>Ayarlar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -20,5 +41,17 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     marginBottom: 20,
+  },
+  buttonContainer: {
+    marginBottom: 20,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 20,
+  },
+  iconButton: {
+    alignItems: 'center',
   },
 });
